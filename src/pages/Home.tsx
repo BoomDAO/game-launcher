b20d44b3cb1e1f34f8fefaca098b608ce9e23424
l0st0 <r.bilina@protonmail.com>
2023-03-15 20:56:25 +0100

    banner and heading

--- src/pages/Home.tsx
+++ src/pages/Home.tsx
@@ -1,7 +1,22 @@
-import React from "react";
-
 const Home = () => {
-  return <div>Home</div>;
+  return (
+    <div>
+      <img
+        src="/banner.png"
+        alt="banner"
+        className="h-96 w-full rounded-card object-cover"
+      />
+
+      <h1 className="my-6 flex flex-wrap gap-3 text-[56px] font-semibold leading-none">
+        <span className="gradient-text">Games</span>
+        <span>hosted in</span>
+        <span className="gradient-text">smart contract canisters</span>
+        <span>on the</span>
+        <span className="gradient-text">ICP</span>
+        <span>blockchain</span>
+      </h1>
+    </div>
+  );
 };
 
 export default Home;
