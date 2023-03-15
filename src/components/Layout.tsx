9ade620bb01d8356cf4522b6474b6365422f8271
l0st0 <r.bilina@protonmail.com>
2023-03-15 20:56:13 +0100

    layout max width

--- src/components/Layout.tsx
+++ src/components/Layout.tsx
@@ -3,7 +3,7 @@ import Navigation from "./Navigation";
 
 const Layout = ({ children }: React.PropsWithChildren) => {
   return (
-    <div className="space-y-6 px-6 py-4">
+    <div className="m-auto w-full max-w-screen-xl space-y-6 px-6 py-4">
       <Navigation />
       {children}
     </div>
