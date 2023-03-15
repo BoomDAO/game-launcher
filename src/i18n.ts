134bf4c514c1e56f2986fe8c5fb6d16a743c2794
l0st0 <r.bilina@protonmail.com>
2023-03-15 20:28:15 +0100

    i18 types

--- src/i18n.ts
+++ src/i18n.ts
@@ -2,20 +2,13 @@ import { initReactI18next } from "react-i18next";
 import i18next from "i18next";
 import common from "./locale/en/common.json";
 
-const defaultNS = "common";
+export const defaultNS = "common";
 
-const resources = {
+export const resources = {
   en: {
     common,
   },
 };
-
-declare module "i18next" {
-  interface CustomTypeOptions {
-    defaultNS: typeof defaultNS;
-    resources: (typeof resources)["en"];
-  }
-}
 
 i18next.use(initReactI18next).init({
   resources,
