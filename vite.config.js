9acc9338c95bc99341df02d6ed7156d09a9d6a39
l0st0 <r.bilina@protonmail.com>
2023-03-18 19:07:10 +0100

    change ports

--- vite.config.js
+++ vite.config.js
@@ -1,8 +1,16 @@
-import { defineConfig } from "vite";
 import react from "@vitejs/plugin-react";
+import { defineConfig } from "vite";
 import tsconfigPaths from "vite-tsconfig-paths";
+
+const port = 3000;
 
 /** @type {import('vite').UserConfig} */
 export default defineConfig({
   plugins: [react(), tsconfigPaths()],
+  server: {
+    port,
+  },
+  preview: {
+    port,
+  },
 });
