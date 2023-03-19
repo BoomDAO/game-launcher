0edcc6e7196145ea7c5978241bfcbc7e32968ee9
l0st0 <r.bilina@protonmail.com>
2023-03-19 09:55:12 +0100

    logo loader on loading session

--- src/components/Layout.tsx
+++ src/components/Layout.tsx
@@ -1,9 +1,15 @@
 import React from "react";
+import { useAuth } from "@/context/authContext";
 import Footer from "./Footer";
+import LogoLoader from "./LogoLoader";
 import Navigation from "./Navigation";
 import Space from "./Space";
 
 const Layout = ({ children }: React.PropsWithChildren) => {
+  const { isLoading } = useAuth();
+
+  if (isLoading) return <LogoLoader />;
+
   return (
     <div className="m-auto flex min-h-screen w-full max-w-screen-xl flex-col px-8 py-6">
       <Navigation />
