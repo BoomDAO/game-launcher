b5f49cae9aaf07f229b725dd44541b9907a0f32b
hitesh <hiteshtripathi12345678@gmail.com>
2023-05-23 12:19:59 +0530

    automation and name of game in banner

--- src/components/Slider.tsx
+++ src/components/Slider.tsx
@@ -2,6 +2,8 @@ import React from "react";
 import { useState } from "react";
 import ArrowCircleLeftRoundedIcon from '@mui/icons-material/ArrowCircleLeftRounded';
 import ArrowCircleRightRoundedIcon from '@mui/icons-material/ArrowCircleRightRounded';
+import "../styles/Slider.css";
+import "../styles/index.css";
 
 export default function Slider() {
     //add new featured games here
@@ -9,15 +11,17 @@ export default function Slider() {
         {
             image: "/banner.png",
             url: "https://5iuic-ryaaa-aaaal-ack7a-cai.raw.icp0.io/",
-
+            name: "Plethora"
         },
         {
             image: "/game-1.png",
             url: "https://fsowo-7aaaa-aaaal-acdxa-cai.raw.icp0.io/",
+            name: "Cubetopia"
         },
         {
             image: "/game-2.png",
             url: "https://edeir-hiaaa-aaaal-acdsq-cai.raw.icp0.io/",
+            name: "Degen Spin"
         },
     ];
     const [activeImageNum, setCurrent] = useState(0);
@@ -31,10 +35,18 @@ export default function Slider() {
     if (!Array.isArray(sliderImages) || sliderImages.length <= 0) {
         return null;
     }
+
+    React.useEffect(() => {
+        const interval = setInterval(() => {
+            nextSlide();
+        }, 5000);
+        return () => clearInterval(interval);
+    }, [activeImageNum]);
+
     return (
         <div>
             <section style={{ position: "relative", justifyContent: "center", alignItems: "center" }}>
-                <div className="left" style={{ position: "absolute", top: "46%", left: "1rem", userSelect: "none", cursor: "pointer", zIndex: "5", color: "white" }}>
+                <div style={{ position: "absolute", top: "46%", left: "1rem", userSelect: "none", cursor: "pointer", zIndex: "5", color: "white" }}>
                     <ArrowCircleLeftRoundedIcon onClick={prevSlide} style={{ fontSize: "3rem" }} />
                 </div>
                 <div className="right" style={{ position: "absolute", top: "46%", right: "1rem", userSelect: "none", cursor: "pointer", zIndex: "5", color: "white" }}>
@@ -42,13 +54,16 @@ export default function Slider() {
                 </div>
                 {sliderImages.map((currentSlide, ind) => {
                     return (
-                        <div
-                            className={ind === activeImageNum ? "currentSlide active" : "currentSlide"}
-                            key={ind}
-                        >
-                            <a href={currentSlide.url} className="cursor: pointer" target="_blank">
-                                {ind === activeImageNum && <img src={currentSlide.image} className="h-72 w-full rounded-primary object-cover shadow md:h-96" />}
-                            </a>
+                        <div>
+                            <div
+                                className={ind === activeImageNum ? "currentSlide active" : "currentSlide"}
+                                key={ind}
+                            >
+                                <a href={currentSlide.url} className="cursor: pointer" target="_blank">
+                                    {ind === activeImageNum && <img src={currentSlide.image} className="h-72 w-full rounded-primary object-cover shadow md:h-96" />}
+                                    {ind === activeImageNum && <p className="absolute text-yellow-400 z-10 cursor-pointer text-6xl font-bold" style={{ top: "75%", right: "4rem", fontFamily:'Poppins'}}>{currentSlide.name}</p>}
+                                </a>
+                            </div>
                         </div>
                     );
                 })}
